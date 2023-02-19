b9aef24cdb8e0d38bac822de1cf179daedcb67fb
Harminder Virk <virk.officials@gmail.com>
2023-02-19 11:00:19 +0530

    test: add test for page.use method

--- tests/interaction.spec.ts
+++ tests/interaction.spec.ts
@@ -13,6 +13,7 @@ import { chromium } from 'playwright'
 import { BasePage } from '../src/base_page'
 import { decorateBrowser } from '../src/browser'
 import { ServerFactory } from '../factories/server'
+import { addUseMethod } from '../src/decorators/use'
 import { addVisitMethod } from '../src/decorators/visit'
 import { BaseInteraction } from '../src/base_interaction'
 
@@ -60,5 +61,45 @@ test.group('Interaction', () => {
     const context = await browser.newContext()
     const page = await context.visit(HomePage)
     await page.form.checkTerms().assertIsChecked()
+  })
+
+  test('mount interaction to an existing page', async ({ assert, cleanup }) => {
+    const server = new ServerFactory()
+    await server.create((_, res) => {
+      res.setHeader('content-type', 'text/html')
+      res.write(`<html>
+        <head>
+          <title> Hello world </title>
+        </head>
+        <body>
+          <div>
+            <input type="checkbox" name="terms" /> Terms and conditions
+          </div>
+        </body>
+      </html>`)
+      res.end()
+    })
+
+    const browser = decorateBrowser(await chromium.launch(), [addVisitMethod, addUseMethod])
+    cleanup(async () => {
+      await browser.close()
+      await server.close()
+    })
+
+    class FormInteraction extends BaseInteraction {
+      checkTerms() {
+        return this.defer(() => this.page.locator('input[name="terms"]').check())
+      }
+
+      assertIsChecked() {
+        return this.defer(async () => {
+          assert.isTrue(await this.page.locator('input[name="terms"]').isChecked())
+        })
+      }
+    }
+
+    const context = await browser.newContext()
+    const page = await context.visit(server.url)
+    await page.use(FormInteraction).checkTerms().assertIsChecked()
   })
 })
