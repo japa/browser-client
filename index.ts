ecbc37820e3c7b994b889773102f3888ec65280b
Harminder Virk <virk.officials@gmail.com>
2023-07-05 09:59:52 +0530

    style: reorganize imports

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@
 import './src/types/extended.js'
 
 export { BasePage } from './src/base/base_page.js'
-export { decorateBrowser } from './src/decorate_browser.js'
 export { browserClient } from './src/plugin/main.js'
+export { decorateBrowser } from './src/decorate_browser.js'
 export { BaseInteraction } from './src/base/base_interaction.js'
 export { decoratorsCollection } from './src/decorators/collection.js'
